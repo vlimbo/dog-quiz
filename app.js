8c3b70186669896c467f9e72b0850c7fc18a84e9
vlimbo <vwlimbo@proton.me>
2024-10-22 10:42:33 +0200

    Add acutal picture of dog into html
    Now it exists an acutal picture of dog with the help of a function inside of JS.

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // variables
 const img = document.querySelector(".main__img");
 const btns = document.querySelectorAll(".main__btn");
+
+// funtions
+const setImg = () => {
+  img.src = "dog1.webp";
+};
+
+// real time functions
+setImg();
